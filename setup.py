a0902497749f72f60467977d2fce0856a31b8f72
Anna Nikulásdóttir <anna@grammatek.com>
2022-03-22 19:29:01 +0000

    add pos dependency

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
 	name="regina-normalizer",
     package_dir={'': 'regina_normalizer_pkg'},
     packages=find_packages(where='regina_normalizer_pkg'),
-	install_requires=['setuptools'],#, 'pos >= v2.1.0'],
+	install_requires=[
+		'setuptools',
+		'pos @ git+https://github.com/cadia-lvl/POS.git@v3.0.0'],
 	python_requires='>=3.5',
-
 	)
