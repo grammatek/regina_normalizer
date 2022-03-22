fc1dd49ae10324248b214adce925cf2656e6814b
Anna Nikulásdóttir <anna@grammatek.com>
2022-03-22 10:19:28 +0000

    edit top-level setup

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
 	name="regina-normalizer",
-    package_dir={'': 'regina_normalizer_pkg/regina_normalizer'},
-    packages=find_packages(where='regina_normalizer_pkg/regina_normalizer'),
+    package_dir={'': 'regina_normalizer_pkg'},
+    packages=find_packages(where='regina_normalizer_pkg'),
 	install_requires=['setuptools'],#, 'pos >= v2.1.0'],
 	python_requires='>=3.5',
-	
+
 	)
